41dfa0f447b390313a4a6a7f7fa57176ad77be84
arber inc <arber.rexhepi@gmail.com>
2024-05-15 23:09:51 -0300

    moved the button js all to header, since they are header buttons just repeated elsewhere in the DOM. seems a little more stable, and this corrected the url params so they all include ?burst. This would be how I'd use this in production anyway so might as well test it under the ideal situation for testing. I am noticing some inconsistencies throughout all deploys between render.com live site and local (where local is a bit more stable and accurate to the code)

--- client/views/documentation/documentation.js
+++ client/views/documentation/documentation.js
@@ -1,9 +1,3 @@
 window.frozenVanilla("documentation", function (vanillaPromise) {
   console.log(vanillaPromise.this + " ran");
-
-  let genButtonRoute = "gen";
-  $("#genbutton-documentation_documentation").on("click", function (e) {
-    let route = this.getAttribute("data-route");
-    window.myState([genButtonRoute, `../${genButtonRoute}`]);
-  });
 });
--- client/views/shared/heroHeader/heroHeader.js
+++ client/views/shared/heroHeader/heroHeader.js
@@ -1,9 +1,11 @@
 window.frozenVanilla("heroHeader", function (vanillaPromise) {
   console.log(vanillaPromise.this + " ran");
 
-  let button = document.querySelector("button.headerbutton");
-  button.addEventListener("click", function (event) {
-    let route = event.target.getAttribute("data-route");
-    window.myState([route, `../?burst=${route}`]);
+  let buttons = document.querySelectorAll("button.headerbutton");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      let route = event.target.getAttribute("data-route");
+      window.myState([route, `../?burst=${route}`]);
+    });
   });
 });
